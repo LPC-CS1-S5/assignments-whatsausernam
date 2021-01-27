71b14cbea32b20fa48682c7e2419b47841334991
Joshua Frazier <joshuafrazier276@gmail.com>
2021-01-27 16:51:23 +0000

    assignment 2-10

--- main.cpp
+++ main.cpp
@@ -1,21 +1,52 @@
 
 
+
+
 #include <iostream>
+
+
 using namespace std;
+
+
+
 
 int main() {
 
-char b;
-b = 65;
-
-cout << b << endl;
 
 
-b = 66;
 
-cout << b << endl;
+ int checking;
 
 
+ unsigned int miles;
+
+
+ long days;
+
+
+
+
+ checking = -20;
+
+
+ miles = 4276;
+
+
+ days = 18900;
+
+
+ cout << "We have made a long journey of " << miles;
+
+
+ cout << " miles." << endl;
+
+
+ cout << "Our checking account balance is " << checking << "." <<endl;
+
+
+ cout << "About " << days << " days ago Columbus stood on this spot" << endl;
 
 }
+
+
 
