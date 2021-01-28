59aacfd5cdb50bb3e475be5a0392de5f93408fbb
Joshua Frazier <joshuafrazier276@gmail.com>
2021-01-28 08:09:46 +0000

    not finished

---
+++ prog_chal1.cpp
@@ -0,0 +1,22 @@
+//all my basic program set up
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+//where i begin the actual code
+int main(){
+
+//setting all my variables
+  double mealCost;
+  double tax;
+  double tip;
+  double total;
+  mealCost = 88.67;
+  tax = mealCost * .675;
+  tip = mealCost * .2;
+  total = mealCost + tax + tip;
+
+cout << mealCost << endl;
+cout << 
+
+}
