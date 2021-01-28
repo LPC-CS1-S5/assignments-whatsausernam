8257a936e617f2b45364010687765c414b4fd03c
Joshua Frazier <joshuafrazier276@gmail.com>
2021-01-28 21:29:39 +0000

    program challenge 2 complete

---
+++ prog_chal2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+
+int main(){
+
+double a = 28;
+double b = 32;
+double c = 37;
+double d = 24;
+double e = 33;
+double sum = a + b + c + d + e;
+double average = sum / 5;
+
+cout << setprecision(3) << "The sum of all the numbers is " << sum << endl;
+cout << "The average of the numbers is " << average << endl;
+
+
+
+
+
+}
