8e83017be2f28eba6ea76736420ecdb1015ca072
Joshua Frazier <joshuafrazier276@gmail.com>
2021-01-28 20:39:43 +0000

    complete

--- prog_chal1.cpp
+++ prog_chal1.cpp
@@ -12,11 +12,13 @@ int main(){
   double tip;
   double total;
   mealCost = 88.67;
-  tax = mealCost * .675;
+  tax = mealCost * .0675;
   tip = mealCost * .2;
   total = mealCost + tax + tip;
 
-cout << mealCost << endl;
-cout << 
+cout << "Your food is $" << mealCost << endl;
+cout << "The tax is $" << setprecision(3) << tax << endl;
+cout << "A 20% tip will be $" << setprecision(4) << tip << endl;
+cout << "In total you owe $" << setprecision(5) << total << endl;
 
 }
