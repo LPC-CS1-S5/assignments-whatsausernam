b74dc90b909498d5671e4fc956ebfd6bf9f8c7e0
repl.it user <replituser@example.com>
2021-02-21 04:26:20 +0000

    Q

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 
 
+<<<<<<< HEAD
 #include <iostream>
 using namespace std;
 
@@ -19,3 +20,55 @@ cout << b << endl;
 
 }
 
+=======
+
+
+#include <iostream>
+
+
+using namespace std;
+
+
+
+
+int main() {
+
+
+
+
+ int checking;
+
+
+ unsigned int miles;
+
+
+ long days;
+
+
+
+
+ checking = -20;
+
+
+ miles = 4276;
+
+
+ days = 18900;
+
+
+ cout << "We have made a long journey of " << miles;
+
+
+ cout << " miles." << endl;
+
+
+ cout << "Our checking account balance is " << checking << "." <<endl;
+
+
+ cout << "About " << days << " days ago Columbus stood on this spot" << endl;
+
+}
+
+
+
+>>>>>>> c18eba53442145c532c23ddf45bdec150251adac
