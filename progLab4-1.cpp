1631e1d8cbd1c79dfacd0eb946643570f543b214
Joshua Frazier <joshuafrazier276@gmail.com>
2021-02-26 20:12:45 +0000

    Lab 4-1 is done srry that its late

---
+++ progLab4-1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+//declare variables
+int a , b = 0;
+
+//ask user to input two unique integers
+cout << "Please enter two unique integers into the sytem." << endl;
+cin  >> a >> b;
+
+//make your logic statements
+if(a>b){
+  cout << a << " is the bigger number and " << b << " is the smaller number." << endl;
+}
+else{
+  cout << b << " is the bigger number and " << a << " is the smaller number." << endl;
+}
+
+
+
+
+
+}
