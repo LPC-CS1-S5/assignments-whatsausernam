281fde1a2e9fb62036aae1e5e7cd453dffc68ff1
Joshua Frazier <joshuafrazier276@gmail.com>
2021-03-10 17:13:22 +0000

    5-3 done

---
+++ quiz5-3.3.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+
+  int begin, end, i, prime;
+
+  cout << "Please enter a lower bound as an integer: ";
+  cin >> begin;
+  cout << "Please enter an upper bound as an integer: ";
+  cin >> end;
+
+  while(begin > end || begin < 0){
+    cout << "Error invalid range please reenter a valid range: " << endl;
+    cout << "Begin: ";
+    cin >> begin;
+    cout << "End: ";
+    cin >> end;
+  }
+  for(i = begin ; i <= end; i++){
+    for(int j = 2; j <= sqrt(i); j++){
+      if(i % j == 0)
+        prime++;
+    }
+    if(prime == 0 && i != 1){
+      cout << i <<" ";
+      prime = 0;
+    }
+    else{
+      prime = 0;
+    }  
+  }
+}
